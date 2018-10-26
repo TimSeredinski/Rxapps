32dcbf0d20b44bd735c27b21006d9dc4de2370af
TimSeredinski <rela1wer@gmail.com>
2018-10-26 17:23:06 +0300

    Added exam work of the first course

---
+++ RXapps/123.js
@@ -0,0 +1,9 @@
+var date = new Date();
+
+var object = new Object;
+
+object.day = date.getDay();
+console.log("Day - " + object.day );
+
+object.month = date.getMonth()+1;
+console.log("Month - " + object.month );
